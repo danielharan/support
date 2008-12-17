d26b3c547c733b9354a989a20937be37addc0bdd
Marc-Andre Lafortune <github@marc-andre.ca>
2008-12-17 01:01:29 -0500

    Tests pass

--- lib/support/hash_ext.rb
+++ lib/support/hash_ext.rb
@@ -4,14 +4,7 @@ module Support
     #   > {:a => 1, :b => 1} + {:a => 1}
     #   => {:a => 2, :b => 1}
     def +(other)
-      (self.keys + other.keys).uniq.each do |key|
-        if [self[key], other[key]].all?
-          self[key] += other[key]
-        else
-          self[key] ||= other[key] # ignore the nil
-        end
-      end
-      self
+      merge(other) {|k, cur, other| cur+other}
     end
   end
 end
