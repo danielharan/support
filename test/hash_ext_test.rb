3d431500f03700b56214f53f85400f0bec88cb27
Marc-Andre Lafortune <github@marc-andre.ca>
2008-12-17 01:00:43 -0500

    Failing tests...

--- test/hash_ext_test.rb
+++ test/hash_ext_test.rb
@@ -10,4 +10,19 @@ class TestReminder < Test::Unit::TestCase
     assert_equal({:a => {:b => 1, :c => 2, :d => 3 }},
                  {:a => {:b => 1, :d => 2}} + {:a => {:c => 2, :d => 1}})
   end
+  
+  def test_plus_doesnt_modify_either_hashes
+    a = {:hello => 2}
+    b = {:world => 42}
+    ignore = a + b
+    assert_equal({:hello => 2}, a)
+    assert_equal({:world => 42}, b)
+  end
+
+  def test_with_default_values
+    a = { :hello => 42}
+    b = Hash.new(100).merge!(:world => 66)
+    assert_equal({ :hello => 42, :world => 66 }, a+b)
+  end
+  
 end
